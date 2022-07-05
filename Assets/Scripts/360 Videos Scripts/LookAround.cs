0faa4771ae13c5da1b7a89c0d0c3dfccdadb8d36
Duy Ho <dhh3hb@mail.umkc.edu>
2022-07-05 00:27:01 -0500

    initial commit

---
+++ Assets/Scripts/360 Videos Scripts/LookAround.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            transform.RotateAround(transform.position, -Vector3.up, Input.GetAxis("Mouse X"));
+            transform.RotateAround(transform.position, Vector3.right, speed * Input.GetAxis("Mouse Y"));
+
+        }
+    }
+}

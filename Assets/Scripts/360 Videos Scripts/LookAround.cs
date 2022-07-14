de720afac6e08344b48cf58a531d1990696c0bfe
Duy Ho <dhh3hb@mail.umkc.edu>
2022-07-14 16:39:20 -0500

    update

--- Assets/Scripts/360 Videos Scripts/LookAround.cs
+++ Assets/Scripts/360 Videos Scripts/LookAround.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class LookAround : MonoBehaviour
 {
-    public float speed = 3f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // public float speed = 3f;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
 
-    // Update is called once per frame
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (Input.GetMouseButton(0)) {
+    //         transform.RotateAround(transform.position, -Vector3.up, Input.GetAxis("Mouse X"));
+    //         transform.RotateAround(transform.position, Vector3.right, speed * Input.GetAxis("Mouse Y"));
+
+    //     }
+    // }
+
+    public float rotationSpeed = 3f;
+
+    float pitch;
+    float yaw;
+
     void Update()
     {
-        if (Input.GetMouseButton(0)) {
-            transform.RotateAround(transform.position, -Vector3.up, Input.GetAxis("Mouse X"));
-            transform.RotateAround(transform.position, Vector3.right, speed * Input.GetAxis("Mouse Y"));
+        if (Input.GetMouseButton(0))
+        {
+            pitch += rotationSpeed * Input.GetAxis("Mouse Y");
+            yaw += rotationSpeed * Input.GetAxis("Mouse X");
 
+            // Clamp pitch:
+            pitch = Mathf.Clamp(pitch, -90f, 90f);
+            // Debug.Log(yaw);
+
+            // Wrap yaw:
+            while (yaw < 0f)
+            {
+                yaw += 360f;
+            }
+            while (yaw >= 360f)
+            {
+                yaw -= 360f;
+            }
+            // Debug.Log(yaw);
+
+            // Set orientation:
+            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
         }
+
     }
 }
